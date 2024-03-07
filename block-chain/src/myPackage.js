facdf1dc0c4ac0dd9d8e958a61c0058a19375f6c
dani001024 <aj9473@naver.com>
2024-03-07 12:25:42 +0900

    blockchain 생성

---
+++ block-chain/src/myPackage.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+// 자바스크립트 파일에 JSDoc 코멘트를 더하면 타입스크립트 처럼 사용 가능
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url 
+ * @returns boolean
+ */
+export function init(config){
+    return true;
+}
+
+/**
+ * Exits the program
+ * @param {number} code 
+ * @returns number
+ */
+export function exit(code){
+    return code + 1;
+}
